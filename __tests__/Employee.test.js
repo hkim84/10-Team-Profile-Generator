9c8d38bef4a55e37e4a1266c24252c5f0e1b5d3c
hkim84 <112454703+hkim84@users.noreply.github.com>
2022-12-02 16:30:24 -0800

    add syntax to employee script and package.json syntax for run test script *7/8 passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,48 @@
 const Employee = require("../lib/Employee");
+
+test('should create a new employee', () => {
+    const e = new Employee();
+    expect(typeof(e)).toBe("object");
+});
+
+test('should set the name by constructor argument', () => {
+    const name = "Hoon";
+    const e = new Employee();
+    expect(e.name).toBe(name);
+});
+
+test('should set id by constructor argument', () => {
+    const testValue = "100";
+    const e = new Employee("nooh", testValue);
+    expect(e.id).toBe(testValue);
+});
+
+test('should set email by constructor argument', () => {
+    const testValue = "test@email.com";
+    const e = new Employee("Nooh", "1", testValue);
+    expect(e.email).toBe(testValue);
+});
+
+test("should get name by getName()", () => {
+    const testValue = "Alice";
+    const e = new Employee(testValue);
+    expect(e.getName()).toBe(testValue);
+  });
+  
+  test("should get id by getId()", () => {
+    const testValue = 100;
+    const e = new Employee("Foo", testValue);
+    expect(e.getId()).toBe(testValue);
+  });
+  
+  test("should get email by getEmail()", () => {
+    const testValue = "test@email.com";
+    const e = new Employee("Foo", 1, testValue);
+    expect(e.getEmail()).toBe(testValue);
+  });
+  
+  test("should getRole()then return \"Employee\"", () => {
+    const testValue = "Employee";
+    const e = new Employee("Nooh", 1, "test@email.com");
+    expect(e.getRole()).toBe(testValue);
+  });
